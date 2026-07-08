45c39cce39232d7d7c21f762c4f03aa6885a912a
Tomiwa Enikuomehin <tommyjay071@gmail.com>
2026-07-08 09:39:49 +0100

    Created reusable React components

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+function Hero() {
+  return (
+    <section>
+      <h1>Welcome to Our Store</h1>
+      <p>Find the best products at amazing prices.</p>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/components/Products.jsx
@@ -0,0 +1,11 @@
+function Products() {
+  return (
+    <section>
+      <h2>Featured Products</h2>
+
+      <p>Products will appear here.</p>
+    </section>
+  );
+}
+
+export default Products;
